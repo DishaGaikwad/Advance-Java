34b8134cba2d3824f09cf1d9d13a6d6b0c37b876
DishaGaikwad <101590165+DishaGaikwad@users.noreply.github.com>
2025-12-05 17:15:55 +0530

    Add files via upload

---
+++ AOPDEMO/src/main/java/com/demo/bean/MyUser.java
@@ -0,0 +1,47 @@
+package com.demo.bean;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyUser {
+	
+	public String tostring() {
+		System.out.println("tostring method called");
+		return"MyUser[name=" +name + "]";
+		
+	}
+	@Value("Rajan")
+	private String name;
+
+	public MyUser() {
+		super();
+	}
+
+	public MyUser(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		System.out.println("in getName method");
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void m1() {
+		System.out.println("in m1 method");
+	}
+	
+	public int m2(int x) {
+		System.out.println("in m2 method "+x);
+		return x+10;
+	}
+	
+	
+
+}
+
+
